faad03956d64019a638deb2069951b1862e01ca7
slk <mskoralli@gmail.com>
2019-12-18 12:08:12 +0530

    Added annotation

--- src/main/java/com/slk/DAO/BranchDAO.java
+++ src/main/java/com/slk/DAO/BranchDAO.java
@@ -2,8 +2,11 @@ package com.slk.DAO;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.slk.model.Branch;
 
+@Repository
 public interface BranchDAO {
 	Branch registerBranch(Branch branch);
 	List<Branch> getAllBranch();
--- src/main/java/com/slk/DAO/LoanDAO.java
+++ src/main/java/com/slk/DAO/LoanDAO.java
@@ -2,8 +2,10 @@ package com.slk.DAO;
 
 import java.util.List;
 
-import com.slk.model.Loan;
+import org.springframework.stereotype.Repository;
 
+import com.slk.model.Loan;
+@Repository
 public interface LoanDAO {
 	Loan addLoan(Loan loan);
 	List<Loan> getAllLoans();
